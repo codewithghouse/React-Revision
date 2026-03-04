2da5533d3cf6188204fcaf84e9583c10d25532f1
codewithghouse <personalghouse@gmail.com>
2026-03-05 01:57:30 +0530

    react js curly braces is done

--- react-revision/src/App.jsx
+++ react-revision/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Button  from './components/button'
+import { Jsx } from './components/Jsx'
 
 function App() {
   return <>
@@ -10,6 +11,7 @@ function App() {
   <div>
     <h1>hi  this is ghouse a backend deveeloper and lead full stack engineer at neominds techhub</h1>
     <Button></Button>
+    <Jsx></Jsx>
   </div>
   </>
 }
---
+++ react-revision/src/components/Jsx.jsx
@@ -0,0 +1,9 @@
+function Jsx(){
+    return (
+        <>
+        <p>this is the calculation of 2 numbers 2*2 = {2*2}</p>
+        </>
+    )
+}
+
+export {Jsx};
